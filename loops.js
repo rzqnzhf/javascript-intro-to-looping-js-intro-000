e02fedead5bced1e172a8c206b624c2bc12a13d7
Skin <rizqinnazhif@gmail.com>
2019-11-26 19:17:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,5 +23,5 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do {
     console.log('I run once regardless.');
-  } while (incrementVariable() < num);
+  } while (incrementVariable() < num - 1);
 }
