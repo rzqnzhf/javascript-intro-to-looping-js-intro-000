889eeaa8f963cff6d62eae66edba5db6b7a11d46
Skin <rizqinnazhif@gmail.com>
2019-11-26 18:36:25 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) {
+    for (var i = 0; i < 25; i++) {
+      var x = `I am ${i} strange loop${i < 2 ? '' : 's'}.`;
+      array.push(x);
+    }
+    return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+  }
+  consoloe.log('done')
+}
